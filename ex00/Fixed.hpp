5a2f8d2a2d8ac19b629ffed4a5e4065d08bbf0e1
Wongoose <wong.zhengxiang@gmail.com>
2023-05-25 10:56:18 +0800

    Started ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,24 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed {
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(const Fixed &fixed);
+		Fixed &operator=(Fixed fixed);
+
+		void swap(Fixed &first, Fixed &second);
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+
+	private:
+		int raw_value_;
+		static const int fractional_bits_ = 8;
+
+};
+
+
+#endif
